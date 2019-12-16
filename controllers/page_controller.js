4779cbdfb28e18624933f28529559073d53deea2
garretblankenship <garret.blankenship@coderacademy.edu.au>
2019-12-16 14:59:22 +1100

    checkpoint 6

--- controllers/page_controller.js
+++ controllers/page_controller.js
@@ -1,6 +1,6 @@
 function index(req, res) {
     req.session.views = req.session.views ? req.session.views + 1 : 1;
-    res.send(`Welcome you have viewed this page ${req.session.views} time(s)`);
+    res.send(`Welcome you have viewed this page ${req.session.views} time`);
 }
 
 function dashboard(req, res) {
